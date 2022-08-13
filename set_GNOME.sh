513a67753cd12dbc3e5445d91341241cafa453dc
Nasheed Ur Rehman <96262883+nasheed-x@users.noreply.github.com>
2022-08-14 00:26:10 +0400

    Upload set_GNOME
    Configures GNOME to my personal preferences.

---
+++ set_GNOME.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Enable Dark Mode
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+#Set Desktop Icons To Small
+gsettings set org.gnome.shell.extensions.ding icon-size 'small'
+
+#Set Desktop Icons Position To Top Left
+gsettings set org.gnome.shell.extensions.ding start-corner 'top-left'
+
+#Move Dock To Bottom
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+
+#Set Dock Icon Size To 26
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size '26'
+
+#Set Clock To 12H Format
+gsettings set org.gnome.desktop.interface clock-format '12h'
+
+#Set Background Color
+gsettings set org.gnome.desktop.background picture-uri none
+gsettings set org.gnome.desktop.background primary-color '#FF0000'
+gsettings set org.gnome.desktop.background color-shading-type 'solid'
